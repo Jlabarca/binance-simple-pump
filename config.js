d0bc0ebbc18a0aaccde0d074f7cc0e9bea524a04
Julio Labarca <jlabarca25@gmail.com>
2021-01-31 03:03:40 -0300

    Better config examples

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
+  api_key: 'YOUR-API-KEY',
+  api_secret: 'YOUR-API-SECRET',
   market_buy_inflation: .14, // This is to make sure your buy order gets in. Sets the market buy to current price + inflation percentage
-  api_key: '',
-  api_secret: '',
-  investment: .1
+  investment: .1 // will buy using 10% of your balance
 };
 
 module.exports = config;
