601dfa40af1192752bd350cd506318d24b996dad
Julio Labarca <jlabarca25@gmail.com>
2021-01-31 05:11:47 -0300

    Added utils

--- utils.js
+++ utils.js
@@ -12,6 +12,77 @@ function ask(query) {
     }))
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+    });
+}  
+
+function toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+          x *= Math.pow(10,e-1);
+          x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+          e -= 20;
+          x /= Math.pow(10,e);
+          x += (new Array(e+1)).join('0');
+      }
+    }
+    return x;
+}
+
+function loadExchangeInfo(binance){
+    return new Promise((resolve, reject) => {
+        binance.exchangeInfo(async (error, data) => {
+            if ( error ) {
+                reject(error);
+            }
+
+            let minimums = {};
+            for ( let obj of data.symbols ) {
+                let filters = {status: obj.status};
+                for ( let filter of obj.filters ) {
+                    if ( filter.filterType == "MIN_NOTIONAL" ) {
+                        filters.minNotional = filter.minNotional;
+                    } else if ( filter.filterType == "PRICE_FILTER" ) {
+                        filters.minPrice = filter.minPrice;
+                        filters.maxPrice = filter.maxPrice;
+                        filters.tickSize = filter.tickSize;
+                    } else if ( filter.filterType == "LOT_SIZE" ) {
+                        filters.stepSize = filter.stepSize;
+                        filters.minQty = filter.minQty;
+                        filters.maxQty = filter.maxQty;
+                    }
+                }
+                filters.orderTypes = obj.orderTypes;
+                filters.icebergAllowed = obj.icebergAllowed;
+                minimums[obj.symbol] = filters;
+            }
+            resolve(minimums);
+        });
+    });
+}
+
+function loadBalances(binance){
+    return new Promise((resolve, reject) => {
+        binance.balance(async (error, data) => {
+            if ( error ) {
+                reject(error);
+            }
+            resolve(data);
+        });
+    });
+}
+
 module.exports = {
-    ask
+    ask,
+    sleep,
+    toFixed,
+    loadExchangeInfo,
+    loadBalances
 };
