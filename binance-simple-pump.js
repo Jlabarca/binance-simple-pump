25b06b1ed5a538cf1520adaeaafd2df69fe541ff
Julio Labarca <jlabarca25@gmail.com>
2021-02-02 01:46:34 -0300

    Added -sell param for sell only mode

--- binance-simple-pump.js
+++ binance-simple-pump.js
@@ -13,21 +13,37 @@ var prices;
 var balances;
 
 async function init(){
-    preload()
-    .then(() => {
-        askAndBuy()
-        .then(() => log.success('Completed'))
+
+    if(process.argv[2] == "-sell")
+        preload(false, false)
+        .then(() => {
+            askAndSell()
+            .then(() => log.success('Completed'))
+            .catch(log.error);
+        })
         .catch(log.error);
-    })
-    .catch(log.error);
+    else
+        preload()
+        .then(() => {
+            askAndBuy()
+            .then(() => log.success('Completed'))
+            .catch(log.error);
+        })
+        .catch(log.error);
 }
 
-async function preload(){
+async function preload(extractExchangeInfo = true, extractPrices = true){
     await binance.useServerTime();
-    log.info("Preloading exchangeInfo");
-    exchangeInfo = await utils.loadExchangeInfo(binance);
-    log.info("Preloading prices");
-    prices = await binance.prices();
+    if(extractExchangeInfo) {
+        log.info("Preloading exchangeInfo");
+        exchangeInfo = await utils.loadExchangeInfo(binance);
+    }
+
+    if(extractPrices) {
+        log.info("Preloading prices");
+        prices = await binance.prices();
+    }
+    
     log.info("Preloading balances");
     balances = await utils.loadBalances(binance);
 }
@@ -72,6 +88,24 @@ async function askAndBuy(){
         balances = await utils.loadBalances(binance);
         var newBalance = balances[currencySymbol].available;
         log.info(`${currencySymbol} change: ${utils.toFixed(newBalance - balance)}`);
+    }
+}
+
+async function askAndSell(){
+    var currencySymbol = await utils.ask("Currency (USDT, BTC, ...): ");
+    currencySymbol = currencySymbol.toUpperCase();
+    var coinSymbol = await utils.ask("Coin to Sell: ");
+    coinSymbol = coinSymbol.toUpperCase();
+    var balance = balances[coinSymbol].available;
+
+    log.info(`${coinSymbol} balance: ${balance}`);
+
+    var shouldSell = await utils.ask("Sell? (type sell if you want to market sell what you bought) ");
+    var symbol = coinSymbol+currencySymbol;
+
+    if(shouldSell.toUpperCase() == "SELL") {
+        await binance.marketSell(symbol, balance);
+        log.success(`Sold`);
     }
 }
 
